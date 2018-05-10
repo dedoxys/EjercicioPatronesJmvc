38e9cf94fdef9af94961dc212cc0b5157fa13afa
dedoxys <pepevelezcuadrado@gmail.com>
2018-05-10 16:27:52 +0200

    creacion clases principales

---
+++ src/ejercicioPatronesNombre/Persona.java
@@ -0,0 +1,44 @@
+package ejercicioPatronesNombre;
+
+public class Persona {
+	
+	private String nombre;
+	private String apellidos;
+	private int anyoNacimiento;
+	private String ciudad;
+	private int telefono;
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+	public String getApellidos() {
+		return this.apellidos;
+	}
+	public void setApellidos(String _apellidos) {
+		this.apellidos = _apellidos;
+	}
+	public int getAnyoNacimiento() {
+		return this.anyoNacimiento;
+	}
+	public void setAnyoNacimiento(int _anyoNacimiento) {
+		this.anyoNacimiento = _anyoNacimiento;
+	}
+	public String getCiudad() {
+		return this.ciudad;
+	}
+	public void setCiudad(String _ciudad) {
+		this.ciudad = _ciudad;
+	}
+	public int getTelefono() {
+		return this.telefono;
+	}
+	public void setTelefono(int _telefono) {
+		this.telefono = _telefono;
+	}
+	
+	
+	
+}
---
+++ src/ejercicioPatronesNombre/Universidad.java
@@ -0,0 +1,35 @@
+package ejercicioPatronesNombre;
+
+public class Universidad {
+	
+	private String nombre;
+	private String localidad;
+	private int anyoFundacion;
+	private String alumnos;
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+	public String getLocalidad() {
+		return this.localidad;
+	}
+	public void setLocalidad(String _localidad) {
+		this.localidad = _localidad;
+	}
+	public int getAnyoFundacion() {
+		return this.anyoFundacion;
+	}
+	public void setAnyoFundacion(int _anyoFundacion) {
+		this.anyoFundacion = _anyoFundacion;
+	}
+	public String getAlumnos() {
+		return this.alumnos;
+	}
+	public void setAlumnos(String _alumnos) {
+		this.alumnos = _alumnos;
+	}
+	
+}
